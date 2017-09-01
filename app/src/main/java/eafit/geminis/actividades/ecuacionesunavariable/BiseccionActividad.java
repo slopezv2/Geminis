6cde09a426c1d77ab0d8c75d4481d821340b3e65
slopezv2 <sebaslv13@hotmail.com>
2017-09-01 16:18:59 -0500

    Creación de jerarquías y arquitectura app

---
+++ app/src/main/java/eafit/geminis/actividades/ecuacionesunavariable/BiseccionActividad.java
@@ -0,0 +1,15 @@
+package eafit.geminis.actividades.ecuacionesunavariable;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import eafit.geminis.R;
+
+public class BiseccionActividad extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_biseccion);
+    }
+}
---
+++ app/src/main/java/eafit/geminis/actividades/ecuacionesunavariable/BusquedasActividad.java
@@ -0,0 +1,15 @@
+package eafit.geminis.actividades.ecuacionesunavariable;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import eafit.geminis.R;
+
+public class BusquedasActividad extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_busquedas);
+    }
+}
