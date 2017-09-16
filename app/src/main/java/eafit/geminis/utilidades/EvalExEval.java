b86608453e1fd7d1416aff75dee60567a7a4d6c2
slopezv2 <sebaslv13@hotmail.com>
2017-09-16 17:14:56 -0500

    Mejora en evaluador de funciones siguiendo patrón Singleton

--- app/src/main/java/eafit/geminis/utilidades/EvalExEval.java
+++ app/src/main/java/eafit/geminis/utilidades/EvalExEval.java
@@ -10,14 +10,20 @@ import java.math.BigDecimal;
  * Clase para llamar al evaluador EvalEx y retornar el resultado en BigDecimal
  */
 public class EvalExEval {
+
+    private static Expression expression;
     /**
      *
      * @param expresion Expresión a evaluar
      * @param numero El valor de x
+     * @param nuevaExpresion Si se requiere un nuevo objeto
      * @return BigDecimal resultado de la evaluación
      */
-    public static BigDecimal evaluar(String expresion, BigDecimal numero){
-        BigDecimal resultado = new Expression(expresion).with("x",numero).eval();
-        return resultado;
+    public static BigDecimal evaluar(String expresion, BigDecimal numero,boolean nuevaExpresion){
+        if(nuevaExpresion|| expresion==null) {
+            expression = new Expression(expresion);
+        }
+        return expression.with("x",numero).eval();
+
     }
 }
