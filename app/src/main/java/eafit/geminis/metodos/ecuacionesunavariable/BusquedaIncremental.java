f00bafc799a1738da35cd78b7f4b0fb39dc53b2f
slopezv2 <sebaslv13@hotmail.com>
2017-09-28 11:40:37 -0500

    Inicio GUI búsquedas incrementales

--- app/src/main/java/eafit/geminis/metodos/ecuacionesunavariable/BusquedaIncremental.java
+++ app/src/main/java/eafit/geminis/metodos/ecuacionesunavariable/BusquedaIncremental.java
@@ -22,26 +22,26 @@ public class BusquedaIncremental {
      * @return Respuesta con alguna de las posibilidades internas
      */
     public static Respuesta metodo(String funcion, BigDecimal x0, BigDecimal delta, int niter){
+        ArrayList<String> iteraciones = new ArrayList<>();
         Respuesta rp;
         if(niter < 0 ){
-            rp = new Respuesta(TipoRespuesta.Error,ErrorMetodo.ERROR_NITER_INCORRECTO);
+            rp = new Respuesta(TipoRespuesta.Error,ErrorMetodo.ERROR_NITER_INCORRECTO,null);
             return rp;
         }
         if(delta.compareTo(BigDecimal.ZERO) < 1){
-            rp = new Respuesta(TipoRespuesta.Error,ErrorMetodo.ERROR_DELTA_INCORRECTO);
+            rp = new Respuesta(TipoRespuesta.Error,ErrorMetodo.ERROR_DELTA_INCORRECTO,null);
             return rp;
         }
-
         BigDecimal fx0 = null;
         EvalExEval evaluador = new EvalExEval();
         try {
             fx0 = evaluador.evaluar(funcion,x0,true);
         } catch (Exception e) {
-            rp = new Respuesta(TipoRespuesta.Error,e.getMessage());
+            rp = new Respuesta(TipoRespuesta.Error,e.getMessage(),null);
             return rp;
         }
         if(fx0.compareTo(BigDecimal.ZERO) == 0){
-            rp = new Respuesta(TipoRespuesta.RAIZ,x0);
+            rp = new Respuesta(TipoRespuesta.RAIZ,x0,null);
             return rp;
         }
         else{
@@ -51,33 +51,36 @@ public class BusquedaIncremental {
             try {
                 fx1 = evaluador.evaluar(funcion,x1,false);
             } catch (Exception e) {
-                rp = new Respuesta(TipoRespuesta.Error,e.getMessage());
+                rp = new Respuesta(TipoRespuesta.Error,e.getMessage(),null);
                 return rp;
             }
             while(((fx0.multiply(fx1)).compareTo(BigDecimal.ZERO) > 0) && (cont <= niter)){
+                String iteracion = cont+" "+x0+" "+fx0;
+                iteraciones.add(iteracion);
                 x0 = x1;
                 fx0 = fx1;
                 x1 = x0.add(delta);
                 try {
                     fx1 = evaluador.evaluar(funcion,x1,false);
                 } catch (Exception e) {
-                    rp = new Respuesta(TipoRespuesta.Error,e.getMessage());
+                    rp = new Respuesta(TipoRespuesta.Error,e.getMessage(),iteraciones);
                     return rp;
                 }
+
                 cont++;
             }
             if(fx1.compareTo(BigDecimal.ZERO) == 0){
-                rp = new Respuesta(TipoRespuesta.RAIZ,x1);
+                rp = new Respuesta(TipoRespuesta.RAIZ,x1,iteraciones);
                 return rp;
             }
             else if(((fx0.multiply(fx1)).compareTo(BigDecimal.ZERO) < 0)){
                 ArrayList<BigDecimal> intervalo = new ArrayList<BigDecimal>();
                 intervalo.add(x0);
                 intervalo.add(x1);
-                rp = new Respuesta(TipoRespuesta.INTERVALO,intervalo,null);
+                rp = new Respuesta(TipoRespuesta.INTERVALO,intervalo,iteraciones);
                 return rp;
             }else{
-                rp = new Respuesta(TipoRespuesta.Error,ErrorMetodo.ERROR_FRACASO_NITERACIONES);
+                rp = new Respuesta(TipoRespuesta.FRACASO,ErrorMetodo.ERROR_FRACASO_NITERACIONES,iteraciones);
                 return rp;
             }
         }
--- app/src/main/java/eafit/geminis/utilidades/Respuesta.java
+++ app/src/main/java/eafit/geminis/utilidades/Respuesta.java
@@ -9,26 +9,26 @@ import java.util.ArrayList;
 
 public class Respuesta {
     private TipoRespuesta tipo;
-    private ArrayList<BigDecimal> iteraciones_valor;
-    private ArrayList<BigDecimal> error;
+    private ArrayList<String> tablaIteraciones;
+    private ArrayList<BigDecimal> intervalo;
     private BigDecimal valor;
     private int niter;
     private String mensaje;
 
-    public Respuesta(TipoRespuesta tipo, BigDecimal valor){
+    public Respuesta(TipoRespuesta tipo, BigDecimal valor, ArrayList<String> tablaIteraciones){
         this.tipo = tipo;
         this.valor = valor;
     }
-    public Respuesta(TipoRespuesta tipo, ArrayList<BigDecimal> iteraciones_valor, ArrayList<BigDecimal> error){
+    public Respuesta(TipoRespuesta tipo, ArrayList<BigDecimal> intervalo, ArrayList<String> tablaIteraciones){
         this.tipo=tipo;
-        this.iteraciones_valor = iteraciones_valor;
-        this.error = error;
+        this.tablaIteraciones = tablaIteraciones;
+        this.intervalo = intervalo;
     }
-    public Respuesta(TipoRespuesta tipo, int niter){
+    public Respuesta(TipoRespuesta tipo, int niter, ArrayList<String> tablaIteraciones){
         this.tipo = tipo;
         this.niter = niter;
     }
-    public Respuesta(TipoRespuesta tipo, String mensaje){
+    public Respuesta(TipoRespuesta tipo, String mensaje, ArrayList<String> tablaIteraciones){
         this.tipo = tipo;
         this.mensaje = mensaje;
     }
