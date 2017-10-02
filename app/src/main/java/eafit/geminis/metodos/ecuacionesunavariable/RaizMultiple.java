ca42beaf247dfb04001c85517c00f1cb2eff73f4
slopezv2 <sebaslv13@gmail.com>
2017-10-01 22:26:32 -0500

    Inicio de Raices Multiples

---
+++ app/src/main/java/eafit/geminis/metodos/ecuacionesunavariable/RaizMultiple.java
@@ -0,0 +1,16 @@
+package eafit.geminis.metodos.ecuacionesunavariable;
+
+import java.math.BigDecimal;
+
+import eafit.geminis.utilidades.Respuesta;
+
+/**
+ * Created by Familialv on 1/10/2017.
+ */
+
+public class RaizMultiple {
+    public static Respuesta metodo(String funcion, BigDecimal Xinf, BigDecimal Xsup, BigDecimal tol, int niter, boolean errorAbsoluto){
+        //TODO
+        return null;
+    }
+}
